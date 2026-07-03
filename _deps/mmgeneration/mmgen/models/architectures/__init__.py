717bfb542e38c31e5bee0da2344a75fb42b8948a
Cool-Rayyyy1 <775224745@qq.com>
2026-07-03 11:02:11 +0800

    whole upodate

---
+++ _deps/mmgeneration/mmgen/models/architectures/__init__.py
@@ -0,0 +1,46 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .arcface import IDLossModel
+from .biggan import (BigGANDeepDiscriminator, BigGANDeepGenerator,
+                     BigGANDiscriminator, BigGANGenerator, SNConvModule)
+from .cyclegan import ResnetGenerator
+from .dcgan import DCGANDiscriminator, DCGANGenerator
+from .ddpm import DenoisingUnet
+from .fid_inception import InceptionV3
+from .lpips import PerceptualLoss
+from .lsgan import LSGANDiscriminator, LSGANGenerator
+from .pggan import (EqualizedLR, EqualizedLRConvDownModule,
+                    EqualizedLRConvModule, EqualizedLRConvUpModule,
+                    EqualizedLRLinearModule, MiniBatchStddevLayer,
+                    PGGANDiscriminator, PGGANGenerator, PGGANNoiseTo2DFeat,
+                    PixelNorm, equalized_lr)
+from .pix2pix import PatchDiscriminator, generation_init_weights
+from .positional_encoding import CatersianGrid, SinusoidalPositionalEmbedding
+from .singan import SinGANMultiScaleDiscriminator, SinGANMultiScaleGenerator
+from .sngan_proj import ProjDiscriminator, SNGANGenerator
+from .wgan_gp import WGANGPDiscriminator, WGANGPGenerator
+
+__all__ = [
+    'DCGANGenerator', 'DCGANDiscriminator', 'EqualizedLR',
+    'EqualizedLRConvModule', 'equalized_lr', 'EqualizedLRLinearModule',
+    'EqualizedLRConvUpModule', 'EqualizedLRConvDownModule', 'PixelNorm',
+    'MiniBatchStddevLayer', 'PGGANNoiseTo2DFeat', 'PGGANGenerator',
+    'PGGANDiscriminator', 'InceptionV3', 'SinGANMultiScaleDiscriminator',
+    'SinGANMultiScaleGenerator', 'CatersianGrid',
+    'SinusoidalPositionalEmbedding',
+    'generation_init_weights', 'PatchDiscriminator', 'ResnetGenerator',
+    'PerceptualLoss', 'WGANGPDiscriminator', 'WGANGPGenerator',
+    'LSGANDiscriminator', 'LSGANGenerator', 'ProjDiscriminator',
+    'SNGANGenerator', 'BigGANGenerator', 'SNConvModule', 'BigGANDiscriminator',
+    'BigGANDeepGenerator', 'BigGANDeepDiscriminator', 'DenoisingUnet',
+    'IDLossModel'
+]
+
+try:
+    from .stylegan import (MSStyleGAN2Discriminator, MSStyleGANv2Generator,
+                           StyleGAN1Discriminator, StyleGAN2Discriminator,
+                           StyleGANv1Generator, StyleGANv2Generator,
+                           StyleGANv3Generator)
+    __all__ += ['MSStyleGAN2Discriminator', 'MSStyleGANv2Generator', 'StyleGAN2Discriminator',
+                'StyleGANv2Generator', 'StyleGANv1Generator', 'StyleGAN1Discriminator', 'StyleGANv3Generator', ]
+except ModuleNotFoundError:
+    pass
